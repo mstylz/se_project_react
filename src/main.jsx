1c99bea317547733e34f34d6db14ee2abd74a984
Mesut Yildiz <80417121+mstylz@users.noreply.github.com>
2025-10-17 19:46:23 -0400

    Import normalize.css and fonts.css in main.jsx
    Added CSS normalization and font styles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import "./vendor/normalize.css";
+import "./vendor/fonts.css"; 
 import "./index.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
