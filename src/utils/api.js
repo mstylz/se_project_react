6f078b44f764be3058cbc6f5b7173bf20f6d9766
Mesut Yildiz <80417121+mstylz@users.noreply.github.com>
2025-12-07 16:43:49 -0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,11 +7,21 @@ export const checkResponse = (res) => {
   return res.json();
 };
 
+// Normalizes ALL items so the app always receives consistent data
+const normalizeItem = (item) => ({
+  ...item,
+  _id: item._id ?? item.id,
+  link: item.link ?? item.imageUrl, // unify image field
+});
+
 export const getItems = () => {
-  return fetch(BASE_URL).then(checkResponse);
+  return fetch(BASE_URL)
+    .then(checkResponse)
+    .then((data) => data.map(normalizeItem));
 };
 
 export const addItem = ({ name, link, weather }) => {
+  // Always send "link" to server
   return fetch(BASE_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -20,7 +30,9 @@ export const addItem = ({ name, link, weather }) => {
       link,
       weather,
     }),
-  }).then(checkResponse);
+  })
+    .then(checkResponse)
+    .then(normalizeItem);
 };
 
 export const deleteItem = (id) => {
