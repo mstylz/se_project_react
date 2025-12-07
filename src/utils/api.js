d7e921870b89f2fefbcdb3a9732e0589245b69e6
Mesut Yildiz <80417121+mstylz@users.noreply.github.com>
2025-12-07 16:40:20 -0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,32 +1,30 @@
 const BASE_URL = "http://localhost:3001/items";
 
+export const checkResponse = (res) => {
+  if (!res.ok) {
+    throw new Error(res.status);
+  }
+  return res.json();
+};
+
 export const getItems = () => {
-    return fetch(BASE_URL).then((res) => {
-        if (!res.ok) throw new Error(res.status);
-        return res.json();
-    });
+  return fetch(BASE_URL).then(checkResponse);
 };
 
 export const addItem = ({ name, link, weather }) => {
-    return fetch(BASE_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            name,
-            link,
-            weather
-        }),
-    }).then((res) => {
-        if (!res.ok) throw new Error(res.status);
-        return res.json();
-    });
+  return fetch(BASE_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name,
+      link,
+      weather,
+    }),
+  }).then(checkResponse);
 };
 
 export const deleteItem = (id) => {
-    return fetch(`${BASE_URL}/${id}`, {
-        method: "DELETE",
-    }).then((res) => {
-        if (!res.ok) throw new Error(res.status);
-        return res.json();
-    });
+  return fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  }).then(checkResponse);
 };
